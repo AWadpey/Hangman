783a113d02cda0b1bbe175279ee2595144d2c098
MistahSanD <wadpey@gmail.com>
2018-11-09 03:16:48 -0800

    game doesnt guess words yet shows me undefined in console

---
+++ assets/javascript/game.js
@@ -0,0 +1,148 @@
+var words = [
+    "pickle",
+    "doofus",
+    "tiny",
+    "evil",
+    "alien",
+    "mumbling",
+    "toxic",
+    "salesman",
+    "robot",
+    "quantum",
+    "farmer",
+    "mustache",
+    "healthy",
+    "maximus",
+    "future",
+    "drunk",
+    "teacher",
+    "cronenberg",
+    "cowboy",
+    "insurance"
+];
+
+
+const maxTries = 7;
+
+var guessedLetters = [];
+var currentWord;
+var rightletter = [];
+var remainingGuesses = 0;
+var hasFinished = false;
+var wins = 0;
+
+// reset variables
+function resetGame() {
+    remainingGuesses = maxTries;
+
+    currentWord = Math.floor(Math.random() * (words.length));
+
+    guessedLetters= [];
+    rightletter = [];
+
+
+    // the chosen word is replaced by _
+    for (var i = 0; i < words[currentWord].length; i++) {
+        rightletter.push("_");
+    }
+
+
+//    hiding left panel img
+    document.getElementById("pressKeyTryAgain").style.cssText= "display: none";
+    document.getElementById("gameover-image").style.cssText= "display: none";
+    document.getElementById("youwin-image").style.cssText= "display: none";
+
+
+    updateDisplay();
+
+};
+
+console.log("Computer Pick: " + currentWord);
+
+
+function updateDisplay() {
+
+    document.getElementById("totalWins").innerText = wins;
+
+
+    // show how many letters have been guessed right so far
+    var rightletterText = "";
+    for (var i = 0; i < rightletter.length; i++) {
+        rightletterText += rightletter[i];
+    }
+
+    document.getElementById("currentWord").innerText = rightletterText;
+    document.getElementById("remainingGuesses").innerText = remainingGuesses;
+    document.getElementById("guessedLetters").innerText = guessedLetters;
+};
+
+// find how many times the letter is used in the word
+function evaluateGuess(letter) {
+    var positions = [];
+
+    // if correct replace _ with actual letter
+    for (var i = 0; i < words[currentWord].length; i++) {
+        if(words[currentWord][i] === letter) {
+            positions.push(i);
+        }
+    }
+// if wrong subtract a guess
+    if(positions.length <= 0) {
+        remainingGuesses--;
+    }
+    else {
+        for(var i = 0; i < positions.length; i++) {
+            rightletter[positions[i]] = letter;
+        }
+    }
+};
+
+function checkWin() {
+    if(rightletter.indexOf("_") === -1) {
+        document.getElementById("youwin-image").style.csstext = "display: block";
+        document.getElementById("pressKeyTryAgain").style.cssText = "display: block";
+        wins++;
+        hasFinished = true;
+    }
+};
+
+function checkLoss() {
+    if(remainingGuesses <= 0) {
+        document.getElementById("gameover-image").style.cssText = "display: block";
+        document.getElementById("pressKeyTryAgain").style.cssText = "display: block";
+        hasFinished = true;
+    }
+};
+
+// make a guess and make sure letter hasn't been used yet
+function makeGuess(letter) {
+    if (remainingGuesses > 0) {
+        if (guessedLetters.indexOf(letter) === -1) {
+            guessedLetters.push(letter);
+            evaluateGuess(letter);
+        }
+    }
+};
+
+// event listener
+document.onkeydown = function(event) {
+    if(hasFinished) {
+        resetGame();
+        hasFinished = false;
+    }
+    else {
+        if(event.keyCode >= 65 && event.keyCode <= 90) {
+            makeGuess(event.key.toUpperCase());
+            updateDisplay();
+            checkWin();
+            checkLoss();
+        } 
+    }
+};
+
+
+
+
+
+
+
